99feff8156a156b45e01a5767035dc4322efe054
Jesper Grip <jesper.brannstrom@ffcg.se>
2020-12-05 10:53:08 +0100

    Refactor method name in day 5.

--- AdventOfCode.2020/AdventOfCode.2020.Day5/SeatIdExtensions.cs
+++ AdventOfCode.2020/AdventOfCode.2020.Day5/SeatIdExtensions.cs
@@ -4,7 +4,7 @@ namespace AdventOfCode._2020.Day5
 {
   public static class SeatIdExtensions
   {
-    public static int ToSeatId(this string s) => s.Aggregate(0, (i, c) => 2 * i + (c.IsBack() ? 1 : 0));
-    private static bool IsBack(this char c) => c % 8 == 2;
+    public static int ToSeatId(this string s) => s.Aggregate(0, (i, c) => 2 * i + (c.IsBackOrRight() ? 1 : 0));
+    private static bool IsBackOrRight(this char c) => c % 8 == 2;
   }
 }
